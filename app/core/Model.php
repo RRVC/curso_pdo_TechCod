385ebdea2024dfdf288722f4f6bc1ae4b0156e56
RRVC <l2decutor@hotmail.com>
2022-01-29 04:44:03 -0300

    Comandos básicos de PDO e configuração da conexão

---
+++ app/core/Model.php
@@ -0,0 +1,10 @@
+<?php
+
+
+try {
+    $pdo = new PDO("mysql:dbname=CRUDPDO;host=localhost", "root", "");
+} catch (PDOException $e) {
+    echo "Erro com banco de dados: " . $e->getMessage();
+} catch (Exception $e) {
+    echo "Erro genérico:" . $e->getMessage();
+}
